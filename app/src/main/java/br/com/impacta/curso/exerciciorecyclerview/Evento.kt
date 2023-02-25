ef97ea82e5be0b1f620797c2cfca66926b2a8f56
Fernando Tanaka <fernando.tomio.tanaka@gmail.com>
2023-02-24 21:34:02 -0300

    data class Evento.kt

---
+++ app/src/main/java/br/com/impacta/curso/exerciciorecyclerview/Evento.kt
@@ -0,0 +1,9 @@
+package br.com.impacta.curso.exerciciorecyclerview
+
+import java.time.LocalDateTime
+
+data class Evento(
+    val titulo: String,
+    val descricao: String,
+    val data: LocalDateTime
+)
