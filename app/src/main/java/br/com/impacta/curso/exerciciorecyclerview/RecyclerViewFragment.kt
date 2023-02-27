8fbe498c28bf1c0a6e9ca20279e08985c0b9721d
Fernando Tanaka <fernando.tomio.tanaka@gmail.com>
2023-02-27 19:34:33 -0300

    implementação do RecyclerViewFragment.kt

--- app/src/main/java/br/com/impacta/curso/exerciciorecyclerview/RecyclerViewFragment.kt
+++ app/src/main/java/br/com/impacta/curso/exerciciorecyclerview/RecyclerViewFragment.kt
@@ -1,59 +1,42 @@
 package br.com.impacta.curso.exerciciorecyclerview
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.com.impacta.curso.exerciciorecyclerview.databinding.FragmentRecyclerViewBinding
+import java.time.LocalDateTime
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RecyclerViewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@RequiresApi(Build.VERSION_CODES.O)
 class RecyclerViewFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentRecyclerViewBinding? = null
+    private val binding get() = _binding!!
+    private val listaEventos: ArrayList<Evento> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recycler_view, container, false)
+        _binding = FragmentRecyclerViewBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecyclerViewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RecyclerViewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listaEventos.add(Evento("Evento 1", "Descricao 1", LocalDateTime.now()))
+        listaEventos.add(Evento("Evento 2", "Descricao 2", LocalDateTime.now()))
+        listaEventos.add(Evento("Evento 3", "Descricao 3", LocalDateTime.of(2023, 1,1, 10,20 )))
+
+        val adaptador = Adaptador(listaEventos)
+        binding.rv.adapter = adaptador
+        binding.rv.layoutManager = LinearLayoutManager(requireContext())
     }
+
 }
