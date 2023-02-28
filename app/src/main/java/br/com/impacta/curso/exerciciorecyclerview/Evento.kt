d39475d07e194f8c63ea0bc78ddda1660f924a4e
Fernando Tanaka <fernando.tomio.tanaka@gmail.com>
2023-02-27 21:10:01 -0300

    novas funções na data class, para converter valores não string em string

--- app/src/main/java/br/com/impacta/curso/exerciciorecyclerview/Evento.kt
+++ app/src/main/java/br/com/impacta/curso/exerciciorecyclerview/Evento.kt
@@ -1,9 +1,17 @@
 package br.com.impacta.curso.exerciciorecyclerview
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.time.LocalDateTime
 
 data class Evento(
     val titulo: String,
     val descricao: String,
     val data: LocalDateTime
-)
+) {
+    fun getDataString() = this.data.toString()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getDiaMes() = "${this.data.dayOfMonth}\n${this.data.month}"
+
+}
